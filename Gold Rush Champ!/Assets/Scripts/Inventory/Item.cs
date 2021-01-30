9f6aaf119516a5afe7851722b2ec32d9cc4c9129
Alec Asperslag <asperslaga@gmail.com>
2021-01-30 11:08:09 -0800

    Create item Scriptable Object
    Also some folders to put things in

---
+++ Gold Rush Champ!/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Item", menuName = "Create Gold Rush Item", order = 1)]
+public class Item : ScriptableObject
+{
+    public string Name;
+    public Sprite ItemImage;
+
+    [TextArea]
+    public string Description;
+
+}
