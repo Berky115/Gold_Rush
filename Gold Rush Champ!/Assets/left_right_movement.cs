f6027d4f36b157e9a572d33922d489125959b5de
Drew <andrewfacchiano@gmail.com>
2021-01-30 12:36:23 -0800

    Re-commiting 2d motion scene

--- Gold Rush Champ!/Assets/left_right_movement.cs
+++ Gold Rush Champ!/Assets/left_right_movement.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class left_right_movement : MonoBehaviour
 {
     public float moveSpeed = 3f;
-    // Use this for initialization
     void Start () {
    
     }
-       void Update ()
-    {
+   
+    void Update ()
+    {                         //Left/Right
         transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal")* moveSpeed);      
     }
 }
