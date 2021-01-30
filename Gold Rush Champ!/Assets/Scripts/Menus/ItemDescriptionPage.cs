87aa544ffeabf6fb15f61e6bb54f3c0dc7450ccc
Brandon Hazelwood <brandonhazelwood532@gmail.com>
2021-01-30 15:03:58 -0800

    Auto stash before merge of "main" and "origin/main"

---
+++ Gold Rush Champ!/Assets/Scripts/Menus/ItemDescriptionPage.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDescriptionPage : MonoBehaviour
+{
+    private static ItemDescriptionPage instance;
+    private Image image;
+    private Text nameText;
+    private Text descriptionText;
+
+
+    //sets initial variables
+    private void Awake()
+    {
+        instance = this;
+        image = transform.Find("Image").GetComponent<Image>();
+        nameText = transform.Find("Item Name").GetComponent<Text>();
+        descriptionText = transform.Find("Item Description").GetComponent<Text>();
+    }
+
+    //sets the item info to be displayed
+    private void ShowItemInfo(Sprite itemSprite, string itemName, string itemDesc)
+    {
+        gameObject.SetActive(true);
+        image.sprite = itemSprite;
+        nameText.text = itemName;
+        descriptionText.text = itemDesc;
+    }
+    
+    //closes window
+    private void HideItemInfo()
+    {
+        gameObject.SetActive(false);
+    }
+
+    //static function for reference elsewhere
+    public static void ShowItem_Static(Sprite itemSprite, string itemName, string itemDesc)
+    {
+        instance.ShowItemInfo(itemSprite, itemName, itemDesc);
+    }
+
+    public static void HideItem_Static()
+    {
+        instance.HideItemInfo();
+    }
+}
