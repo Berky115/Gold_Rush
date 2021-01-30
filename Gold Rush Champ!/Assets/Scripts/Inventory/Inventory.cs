4b0ccbd4f80e34259cbf77bfbf14e664cd05dd5b
Alec Asperslag <asperslaga@gmail.com>
2021-01-30 13:09:24 -0800

    Inventory

---
+++ Gold Rush Champ!/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public Item[] CreateTest;
+
+    //Item Lists
+    public List<Item> FoundItems;
+    public List<Item> ThrownAwayItems;
+    public List<Item> KeptItems;
+
+    public void AddFound(Item foundItem)
+    {
+        FoundItems.Add(foundItem);
+    }
+
+    public void AddThownOut(Item thrownOutItem)
+    {
+        ThrownAwayItems.Add(thrownOutItem);
+    }
+
+    public void AddKeepItem(Item keepItem)
+    {
+        KeptItems.Add(keepItem);
+    }
+
+
+    //private void Start()
+    //{
+    //    //Testing
+    //    //FoundItems.Add(CreateTest);
+    //    //FoundItems.Add(CreateTest);
+
+    //}
+
+
+}
