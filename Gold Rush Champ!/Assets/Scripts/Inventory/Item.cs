b0c27c1290198b0b78b5558ccea0f50acd9cb40e
Alec Asperslag <asperslaga@gmail.com>
2021-01-30 15:34:26 -0800

    Items also changes
    Didn't stage, oops

--- Gold Rush Champ!/Assets/Scripts/Inventory/Item.cs
+++ Gold Rush Champ!/Assets/Scripts/Inventory/Item.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[CreateAssetMenu(fileName = "Item", menuName = "Create Gold Rush Item", order = 1)]
+[CreateAssetMenu(fileName = "Item", menuName = "Gold Rush/Create Item", order = 1)]
 public class Item : ScriptableObject
 {
     public string Name;
