ae362afb260724a505af0ea9b4100e793fc00c05
Drew <andrewfacchiano@gmail.com>
2021-02-12 17:18:32 -0800

    New Trigger Condition for final battle
    Changed scene change to occur once item pile has reached max size. Previous method may cause certain items collected to not be present in final battle.

--- Gold Rush Champ!/Assets/CreepManager.cs
+++ Gold Rush Champ!/Assets/CreepManager.cs
@@ -6,7 +6,11 @@ using UnityEngine.SceneManagement;
 public class CreepManager : MonoBehaviour
 {
     public float CreepyLevel = 1.0f;
+    public const int MAX_PILE_SIZE = 15;
+    // Original method for scene transition. May have use later.
     public float creepThreshold = 7.0f;
+
+    public int pileSize = 0;
     private GameObject player;
     void Start()
     {
@@ -15,11 +19,12 @@ public class CreepManager : MonoBehaviour
 
     public void increaseCreepLevel(float increaseValue){
         CreepyLevel += increaseValue;
-        checkCreepyLevel();
+        pileSize++;
+        checkPileSize();
     }
 
-    public void checkCreepyLevel(){
-        if(CreepyLevel > creepThreshold){
+    public void checkPileSize(){
+        if(pileSize > MAX_PILE_SIZE){
         Debug.Log("Transition to FinalBattle");
             AkSoundEngine.PostEvent("Stop_Music", player);
             AkSoundEngine.PostEvent("Stop_Ambience", player);
